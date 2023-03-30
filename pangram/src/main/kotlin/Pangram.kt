dfc051afdbbbfd978f12ee49bd09899cc8c5ec31
João <dev.junqueira@gmail.com>
2023-03-29 22:59:38 -0300

    Pangram rewrite in a better way

--- pangram/src/main/kotlin/Pangram.kt
+++ pangram/src/main/kotlin/Pangram.kt
@@ -1,22 +1,3 @@
 object Pangram {
-    fun isPangram(input: String): Boolean {
-        val alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
-            .split(",")
-        var sum = 0
-        for (word in alphabet) {
-            when (input.uppercase().any { it == word[0] }) {
-                true -> sum += 1
-                else -> {}
-            }
-        }
-
-        return when (sum) {
-            26 -> true
-            else -> false
-        }
-    }
-}
-
-fun main() {
-    Pangram.isPangram("The quick brown fox jumps over the lazy dog.")
+    fun isPangram(str: String) = ('a'..'z').all { str.contains(it, ignoreCase = true) }
 }
